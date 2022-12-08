35bb15188e5544ce77a09ded6e013ea4386fc03c
baronk2 <kevinbaron14@comcast.net>
2022-12-08 02:25:26 -0800

    Include interactive map

--- child-mortality/app_server.R
+++ child-mortality/app_server.R
@@ -8,6 +8,289 @@
 #
 
 library(shiny)
+library("dplyr")
+library("ggplot2")
+library("ggiraph")
+library("RColorBrewer")
+
+
+
+# Read in data from WHO (previously stored to repo file)
+# Remove any geographic entities that don't have ISO codes
+# Select relevant data
+
+
+all_causes_relevant <-
+  read.csv(
+    "./data/00-all-causes.csv"
+  ) %>% 
+  select(
+    YEAR..CODE.,
+    COUNTRY..DISPLAY.,
+    COUNTRY..CODE.,
+    AGEGROUP..DISPLAY.,
+    CHILDCAUSE..DISPLAY.,
+    Display.Value
+  ) %>% 
+  rename(
+    year = YEAR..CODE.,
+    iso_code = COUNTRY..CODE.,
+    country = COUNTRY..DISPLAY.,
+    age = AGEGROUP..DISPLAY.,
+    cause = CHILDCAUSE..DISPLAY.,
+    deaths = Display.Value
+  ) %>% 
+  filter(
+    iso_code != "",
+    !is.na(
+      iso_code
+    )
+  ) %>% 
+  filter(
+    age == "0-4 years"
+  ) %>% 
+  mutate(
+    country = trimws(
+      country
+    )
+  )
+
+
+
+# Read in landmass (country) shapes (file previously stored to repo file)
+# Rename `region` column to `country` to match WHO's dataframe for joining
+# Remove whitespace from records in `subregion` column since some was found,
+# which would interfere with string matching
+# Rename certain countries and modify associated subregions to match country
+# names as they appear in WHO dataframe.
+
+country_shapes <-
+  read.csv(
+    "./data/country_shapes_original.csv"
+  ) %>%
+  rename(
+    country = region
+  ) %>%
+  mutate(
+    subregion = trimws(
+      subregion
+    ),
+    country = trimws(
+      country
+    )
+  ) %>% 
+  mutate(
+    subregion = case_when(
+      country == "Antigua" ~ "Antigua",
+      country == "Barbuda" ~ "Barbuda",
+      country == "Nevis" ~ "Nevis",
+      country == "Saint Kitts" ~ "Saint Kitts",
+      country == "Trinidad" ~ "Trinidad",
+      country == "Tobago" ~ "Tobago",
+      country == "Saint Vincent" ~ "Saint Vincent",
+      TRUE ~ subregion
+    )
+  ) %>% 
+  mutate(
+    country = case_when(
+      country == "Antigua" ~ "Antigua and Barbuda",
+      country == "Barbuda" ~ "Antigua and Barbuda",
+      country == "Bolivia" ~ "Bolivia (Plurinational State of)",
+      country == "Brunei" ~ "Brunei Darussalam",
+      country == "Ivory Coast" ~ "Côte d'Ivoire",
+      country == "Republic of Congo" ~ "Congo",
+      country == "Cape Verde" ~ "Cabo Verde",
+      country == "Czech Republic" ~ "Czechia",
+      country == "Micronesia" ~ "Micronesia (Federated States of)",
+      country == "UK" ~ "United Kingdom of Great Britain and Northern Ireland",
+      country == "Iran" ~ "Iran (Islamic Republic of)",
+      country == "Nevis" ~ "Saint Kitts and Nevis",
+      country == "Saint Kitts" ~ "Saint Kitts and Nevis",
+      country == "South Korea" ~ "Republic of Korea",
+      country == "Laos" ~ "Lao People's Democratic Republic",
+      country == "Moldova" ~ "Republic of Moldova",
+      country == "North Korea" ~ "Democratic People's Republic of Korea",
+      country == "Russia" ~ "Russian Federation",
+      country == "Swaziland" ~ "Eswatini",
+      country == "Syria" ~ "Syrian Arab Republic",
+      country == "Trinidad" ~ "Trinidad and Tobago",
+      country == "Tobago" ~ "Trinidad and Tobago",
+      country == "Turkey" ~ "Türkiye",
+      country == "Tanzania" ~ "United Republic of Tanzania",
+      country == "USA" ~ "United States of America",
+      country == "Grenadines" ~ "Saint Vincent and the Grenadines",
+      country == "Saint Vincent" ~ "Saint Vincent and the Grenadines",
+      country == "Venezuela" ~ "Venezuela (Bolivarian Republic of)",
+      country == "Vietnam" ~ "Viet Nam",
+      TRUE ~ country
+    )
+  )
+
+
+
+# Function that returns the map as a ggplot object with other layers and specs
+
+get_world_deaths_map <-
+  function(
+    all_causes_all_years_deaths,
+    country_shapes,
+    start_year,
+    end_year,
+    cause_of_death
+  ) {
+    
+    # Filter the data to be between `start_year` and `end_year` and by cause
+    period_cause_deaths <- 
+      all_causes_all_years_deaths %>% 
+      filter(
+        year >= start_year,
+        year <= end_year,
+        cause == cause_of_death
+      ) %>% group_by(
+        country
+      ) %>%
+      summarize(
+        country = country,
+        years = paste0(
+          start_year,
+          "-",
+          end_year
+        ),
+        cause = cause,
+        deaths = sum(
+          deaths,
+          na.rm = TRUE
+        ),
+        tooltip_text = paste0(
+          country,
+          "\n",
+          years,
+          "\n",
+          prettyNum(
+            deaths,
+            big.mark = ","
+          ),
+          " child deaths from ",
+          cause
+        )
+      ) %>% 
+      distinct(
+        country,
+        years,
+        deaths,
+        tooltip_text
+      )
+    
+    country_shapes_period_cause_deaths <- 
+      country_shapes %>% 
+      left_join(
+        period_cause_deaths,
+        by = "country"
+      )
+    
+    
+    # Function for setting the aesthetics of the plot
+    my_theme <- function () {
+      theme_bw() + 
+        theme(
+          plot.title = element_text(
+            hjust=0.5
+          ),
+          plot.subtitle = element_text(
+            hjust=0.5
+          ),
+          axis.title = element_blank(),
+          axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.background = element_blank(),
+          legend.position = "right",
+          legend.title.align = 0.5,
+          panel.border = element_blank(),
+          strip.background = element_rect(
+            fill = 'white',
+            color = 'white'
+          )
+        )
+    }
+    
+    
+    # Specify the plot for the world map
+    
+    map <- ggplot() +
+      geom_polygon_interactive(
+        data = subset(
+          country_shapes_period_cause_deaths,
+          # lat >= -60 & lat <= 90
+        ),
+        color = 'gray70',
+        size = 0.1,
+        aes(
+          x = long,
+          y = lat,
+          fill = deaths,
+          group = group,
+          tooltip = tooltip_text
+        )
+      ) +
+      
+      scale_fill_gradientn(
+        colors = rev(
+          brewer.pal(
+            5,
+            "Spectral"
+          )
+        ),
+        na.value = 'white'
+      ) +
+      
+      
+      labs(
+        fill = "Deaths",
+        title = paste0(
+          "Child Deaths (Under Age 5) From ",
+          cause_of_death
+        ),
+        subtitle = paste0(
+          start_year,
+          "-",
+          end_year
+        ),
+        x = NULL,
+        y = NULL
+      ) +
+      
+      my_theme()
+    
+    return(
+      map
+    )
+  }
+
+
+# Function that returns a `girafe()` object, which will allow the map to be
+# interactive like a `plotly` plot
+
+get_interactive_map <-
+  function(
+    world_child_deaths_map
+  ) {
+    interactive_map <- 
+      girafe(
+        ggobj = world_child_deaths_map,
+        width_svg = 7,
+        height_svg = 4
+      )
+    
+    return(
+      interactive_map
+    )
+  }
+
+
+
+
 
 server <- function(input, output) {
     
@@ -28,6 +311,19 @@ server <- function(input, output) {
   # Section for any Map server elements to avoid merge conflicts
   
   
+  output$child_deaths_map <-
+    renderGirafe({
+      get_interactive_map(
+        get_world_deaths_map(
+          all_causes_relevant,
+          country_shapes,
+          input$years[[1]],
+          input$years[[2]],
+          input$cause
+        )
+      )
+      
+    })
   
   
   
--- child-mortality/tabs/tab_panel_map.R
+++ child-mortality/tabs/tab_panel_map.R
@@ -1,6 +1,7 @@
 # tab_panel_map
 
 library(shiny)
+library("ggiraph")
 
 # Expected Project Structure Description
 # from
@@ -42,6 +43,97 @@ library(shiny)
 
 
 tab_panel_map <-tabPanel(
-    "Map",
-    p("This is a map.")
+    # "Map",
+    # p("This is a map.")
+    
+  
+  "Map",
+  
+  titlePanel(
+    "Map"
+  ),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      
+      # For user to select which years the map will display data for
+      
+      sliderInput(
+        "years",
+        label = h3(
+          "Year Range"
+        ),
+        min = 2000, 
+        max = 2017,
+        value = c(
+          2005,
+          2010
+        ),
+        ticks = FALSE,
+        sep = ""
+      ),
+      
+      # For user to select which Cause of Death the map will display
+      
+      selectInput(
+        "cause",
+        label = h3(
+          "Cause of Death"
+        ),
+        choices = list(
+          "HIV/AIDS" = "HIV/AIDS",
+          "Diarrhoeal diseases" = "Diarrhoeal diseases",
+          "Measles" = "Measles",
+          "Meningitis/encephalitis" = "Meningitis/encephalitis",
+          "Malaria" = "Malaria",
+          "Acute lower respiratory infections" = "Acute lower respiratory infections",
+          "Prematurity" = "Prematurity",
+          "Birth asphyxia and birth trauma" = "Birth asphyxia and birth trauma",
+          "Sepsis and other infectious conditions of the newborn" = "Sepsis and other infectious conditions of the newborn",
+          "Other communicable, perinatal and nutritional conditions" = "Other communicable, perinatal and nutritional conditions",
+          "Congenital anomalies" = "Congenital anomalies",
+          "Other noncommunicable diseases" = "Other noncommunicable diseases",
+          "Injuries" = "Injuries"
+        ), 
+        selected = "HIV/AIDS"
+      )
+      
+    ),
+    
+    mainPanel(
+      
+      girafeOutput("child_deaths_map"),
+      
+      
+      # Map caption
+      
+      p(
+        paste0(
+          "This map shows how many child deaths (age under 5 years old) have ",
+          "occurred in any given country for a selected time set of years ",
+          "between 2000 and 2017 and a selected cause of death. Using the map, ",
+          "you can easily see which countries have had more deaths and which have ",
+          "had fewer deaths using their color and the color scale to the right ",
+          "of the map. You can also hover over a country to see the exact number ",
+          "of recorded deaths for the time period and cause."
+        )
+      ),
+      
+      p(
+        "Source: ",
+        a(
+          "World Health Organization (WHO)",
+          href = "https://www.who.int/data/gho/data/themes/topics/topic-details/GHO/child-mortality-and-causes-of-death"
+        )
+        
+      )
+      
+    )
+    
+  )
+  
+    
+    
+    
 )
